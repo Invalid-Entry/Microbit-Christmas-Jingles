36336cbd3787777f4574c16def4a349ff9987b79
Jmons <jmons@users.noreply.github.com>
2020-12-20 20:10:26 +0000

    Adding in the basic jinglebells

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    LineOne()
+})
+function LineOne () {
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+}
 basic.forever(function () {
 	
 })
